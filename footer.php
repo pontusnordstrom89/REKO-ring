c84a5425fb1181aab740ab99ad28131cd7f3732f
filipornling <81147512+filipornling@users.noreply.github.com>
2022-10-19 17:36:33 +0200

    Tillfällig footer

--- footer.php
+++ footer.php
@@ -13,7 +13,32 @@
 
 </div> <!-- Opened in header -->
 
-
+        <footer class="green lighten-1 page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Utlämning</h5>
+                <p class="white-text text-lighten-1">Fredagar 17:15-18:00</p>
+                <p class="white-text text-lighten-1">Fackklubbsvägen 2</p>
+                <p class="white-text text-lighten-1">Värpinge gårdsbutik/golfbanas parkering</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Länkar</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Länk 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Länk 3 </a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Länk 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2022 Copyright Text
+            </div>
+          </div>
+        </footer>
 
 
 <?php
@@ -23,10 +48,12 @@
  * wp_enqueue_scripts hooks into wp_head and wp_footer and enques scripts & styles
  * @link https://developer.wordpress.org/reference/hooks/wp_head/
  */
-
 wp_footer();
-
 ?>
-</body>
 
+
+</body>
+    <!-- Body starts here -->
+
+ 
 </html>
