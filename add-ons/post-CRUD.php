b0293d03610ee4f8568504f9be559a2135d10454
Pontus Nordström <pontusnordstrom89@gmail.com>
2022-11-03 18:41:49 +0100

    force creaton of pages create-post and order-view and assign them templates, added styling for create post form and moved create post logic to post-CRUD.php

---
+++ add-ons/post-CRUD.php
@@ -0,0 +1,80 @@
+<?php
+$post_id = 0;
+
+if (isset($_POST['title'], $_POST['desc'])) {
+    $title = $_POST['title'];
+    $distance = $_POST['distance'];
+    print_r($distance);
+    $desc = $_POST['desc'];
+    if (is_user_logged_in()) {
+        $userid = get_current_user_id();
+        $args = array(
+            'post_title' => $title,
+            'post_content' => $desc,
+            'post_status' => 'publish',
+            'post_author' => $userid,
+            'post_type' => 'post',
+            'meta_input' => array(
+                'distance_to_delivery' => $distance
+            )
+        );
+        print_r($args);
+        $post_id = wp_insert_post($args);
+    } else {
+        echo 'Please log in to use this form!';
+    }
+}
+
+// Check that the nonce is valid, and the user can edit this post.
+if (
+    isset($_POST['my_image_upload_nonce'], $post_id)
+    && wp_verify_nonce($_POST['my_image_upload_nonce'], 'my_image_upload')
+    && current_user_can('edit_post', $post_id)
+) {
+    // The nonce was valid and the user has the capabilities, it is safe to continue.
+
+    // These files need to be included as dependencies when on the front end.
+    require_once(ABSPATH . 'wp-admin/includes/image.php');
+    require_once(ABSPATH . 'wp-admin/includes/file.php');
+    require_once(ABSPATH . 'wp-admin/includes/media.php');
+
+    // Let WordPress handle the upload.
+    // Remember, 'my_image_upload' is the name of our file input in our form above.
+    $files = $_FILES["my_image_upload"];
+    foreach ($files['name'] as $key => $value) {
+        if ($files['name'][$key]) {
+            $file = array(
+                'name' => $files['name'][$key],
+                'type' => $files['type'][$key],
+                'tmp_name' => $files['tmp_name'][$key],
+                'error' => $files['error'][$key],
+                'size' => $files['size'][$key]
+            );
+            $_FILES = array("my_image_upload" => $file);
+            foreach ($_FILES as $file => $array) {
+                $newupload = media_handle_upload($file, $post_id);
+            }
+        }
+    }
+    $post_permalink = get_permalink($post_id);
+    // redirect the user to the appropriate page
+    wp_redirect($post_permalink);
+    // When finished, die(); is required.
+    die();
+
+    
+    //$attachment_id = media_handle_upload( 'my_image_upload', $post_id );
+
+    //if (is_wp_error()) {
+        // There was an error uploading the image.
+        
+    //} else {
+        // The image was uploaded successfully!
+    //}
+} else {
+    // The security check failed, maybe show the user an error.
+}
+
+/* Showcase image */
+$media = get_attached_media('image', $post_id); // Get image attachment(s) to the current Post
+//print_r($media);
--- template-parts/createPost.php
+++ template-parts/createPost.php
@@ -19,107 +19,57 @@
  */
 get_header();
 ?>
+<div class="row white">
+    <h4>Skapa en annons:</h4>
+    <form class="col s12" id="featured_upload" method="post" action="<?php echo home_url() . '/wp-admin/admin-post.php'; ?>" enctype="multipart/form-data">
 
-<form id="featured_upload" method="post" action="#" enctype="multipart/form-data">
-    <label for="title">Titel</label>
-    <input required type="text" name="title" id="title" placeholder="Titel"/>
+        <!-- Send to function update_profile -->
+        <input type="hidden" name="action" value="create_post">
 
-    <label for="distance">Distans till utlämning</label>
-    <input required type="text" name="distance" id="distance" placeholder="Distans till utlämning"/>
 
-    <label for="desc">Beskrivning</label>
-    <!--<textarea type="text" name="desc" id="desc" placeholder="Skriv din annons här"></textarea> -->
-    <?php wp_editor("", "desc", array(
-        'media_buttons' => FALSE,
-        'quicktags' => FALSE
-    )) ?>
+        <div class="row">
+            <div class="input-field col s6">
+                <input class="validate" type="text" name="title" id="title" required>
+                <label for="title">Titel</label>
+            </div>
 
-    <br><br>
+            <div class="input-field col s6">
+                <input class="validate" type="text" name="distance" id="distance" required>
+                <label for="distance">Distans till utlämning</label>
+            </div>
+        </div>
 
-    <input required type="file" name="my_image_upload[]" id="my_image_upload"  multiple="multiple"/>
-	<?php wp_nonce_field( 'my_image_upload', 'my_image_upload_nonce' ); ?>
+        <p>Beskrivning av din annons</p>
+        <div class="row">
+            <div class="input-field col s12">
+                <label for="desc">Beskrivning</label>
+                <!--<textarea type="text" name="desc" id="desc" placeholder="Skriv din annons här"></textarea> -->
+                <?php wp_editor("", "desc", array(
+                    'media_buttons' => FALSE,
+                    'quicktags' => FALSE
+                )) ?>
 
-    <br><br>
+            </div>
+        </div>
 
-    <input id="submit_my_image_upload" name="submit_my_image_upload" type="submit" value="Publicera Annons">
-</form>
 
-<?php
-$post_id = 0;
-
-if(isset($_POST['title'], $_POST['desc'])) {
-    $title = $_POST['title'];
-    $distance = $_POST['distance'];
-    print_r($distance);
-    $desc = $_POST['desc'];
-    if ( is_user_logged_in() ) {
-        $userid = get_current_user_id();
-        $args = array(
-            'post_title' => $title,
-            'post_content' => $desc,
-            'post_status' => 'publish',
-            'post_author' => $userid,
-            'post_type' => 'post',
-            'meta_input' => array(
-                'distance_to_delivery' => $distance
-            )
-        );
-        print_r($args);
-        $post_id = wp_insert_post($args);
-    } else {
-        echo 'Please log in to use this form!';
-    }
-
-}
-
-// Check that the nonce is valid, and the user can edit this post.
-if ( 
-	isset( $_POST['my_image_upload_nonce'], $post_id ) 
-	&& wp_verify_nonce( $_POST['my_image_upload_nonce'], 'my_image_upload' )
-	&& current_user_can( 'edit_post', $post_id )
-) {
-	// The nonce was valid and the user has the capabilities, it is safe to continue.
-
-	// These files need to be included as dependencies when on the front end.
-	require_once( ABSPATH . 'wp-admin/includes/image.php' );
-	require_once( ABSPATH . 'wp-admin/includes/file.php' );
-	require_once( ABSPATH . 'wp-admin/includes/media.php' );
-	
-    // Let WordPress handle the upload.
-	// Remember, 'my_image_upload' is the name of our file input in our form above.
-    $files = $_FILES["my_image_upload"];
-    foreach ($files['name'] as $key => $value) {
-            if ($files['name'][$key]) { 
-                $file = array( 
-                    'name' => $files['name'][$key],
-                    'type' => $files['type'][$key], 
-                    'tmp_name' => $files['tmp_name'][$key], 
-                    'error' => $files['error'][$key],
-                    'size' => $files['size'][$key]
-                ); 
-                $_FILES = array ("my_image_upload" => $file); 
-                foreach ($_FILES as $file => $array) {
-                    $newupload = media_handle_upload($file, $post_id); 
-                }
-            } 
-        }
-	//$attachment_id = media_handle_upload( 'my_image_upload', $post_id );
-	
-	if ( is_wp_error( $attachment_id ) ) {
-		// There was an error uploading the image.
-	} else {
-		// The image was uploaded successfully!
-	}
-
-} else {
-	// The security check failed, maybe show the user an error.
-}
-
-/* Showcase image */
-$media = get_attached_media('image', $post_id); // Get image attachment(s) to the current Post
-//print_r($media);
-
-?>
+        <div class="row">
+            <div class="file-field input-field">
+                <p>Välj flera bildfiler. Dessa bilder visas i bildspel på din annons</p>
+                <div class="btn btn-reko">
+                    <span>Bläddra</span>
+                    <input type="file" name="my_image_upload[]" id="my_image_upload" multiple="multiple" required>
+                </div>
+                <div class="file-path-wrapper">
+                    <input class="file-path validate" type="text">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <?php wp_nonce_field('my_image_upload', 'my_image_upload_nonce'); ?>
+            <button class="btn waves-effect waves-light btn-reko" id="submit_my_image_upload" name="submit_my_image_upload" type="submit">Publicera annons</button>
+        </div>
+    </form>
 </div>
 
 <?php
@@ -127,5 +77,5 @@ $media = get_attached_media('image', $post_id); // Get image attachment(s) to th
  * get_footer @link https://developer.wordpress.org/reference/functions/get_footer/
  *
  * Looks for footer.php file if no parameter is passed
- */ 
+ */
 get_footer(); ?>
