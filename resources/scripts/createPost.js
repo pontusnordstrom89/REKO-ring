8b6e34217e9e80760c2c5e8980c2fa0d8ad87383
Pontus Nordström <pontusnordstrom89@gmail.com>
2022-12-10 10:50:13 +0100

    bugfix

--- resources/scripts/createPost.js
+++ resources/scripts/createPost.js
@@ -7,8 +7,14 @@
 let newPostFormSubmit = document.getElementById('submit_my_image_upload')
 newPostFormSubmit.addEventListener('click', function () {
 
-    if ($("#featured_upload").valid()) {
-        document.getElementById('createPostLoader').style.display = 'block';
+    document.getElementById('createPostLoader').style.display = 'block';
+
+    var testSleep = function () {
+        setTimeout(function () {
+            document.getElementById('createPostLoader').style.display = 'none';
+        }, 5000);
     }
-    
+
+    testSleep();
+
 })
