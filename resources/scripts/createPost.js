4949853d715b142e7a531e7bcf52a25a93396a4e
Pontus Nordström <pontusnordstrom89@gmail.com>
2022-12-10 13:42:06 +0100

    createPost script updated

--- resources/scripts/createPost.js
+++ resources/scripts/createPost.js
@@ -1,20 +1,20 @@
 
-
-
-
-
 // adds a progressbar to modal after submit
 let newPostFormSubmit = document.getElementById('submit_my_image_upload')
-newPostFormSubmit.addEventListener('click', function () {
+newPostFormSubmit.addEventListener('click', function (event) {
+    
+    // Get required form input fields
 
-    document.getElementById('createPostLoader').style.display = 'block';
+    let title = document.getElementById('title')
+    let text = document.getElementById('my_image_upload')
 
-    var testSleep = function () {
-        setTimeout(function () {
-            document.getElementById('createPostLoader').style.display = 'none';
-        }, 5000);
+    // Check valivity
+    let x = title.checkValidity()
+    let y = text.checkValidity()
+    
+    // If valid
+    if (x && y) {
+        document.getElementById('createPostLoader').style.display = 'block';
     }
-
-    testSleep();
 
 })
